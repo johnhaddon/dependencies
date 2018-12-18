36ed982b1a9773c8de4e990462036812caa2e28d
John Haddon <thehaddonyoof@gmail.com>
2018-12-18 15:44:28 +0000

    Qt : Update to version 5.12
    Also move to config-based build.

---
+++ Qt/config.py
@@ -0,0 +1,72 @@
+{
+
+	"downloads" : [
+
+		"http://download.qt.io/official_releases/qt/5.12/5.12.0/single/qt-everywhere-src-5.12.0.tar.xz"
+
+	],
+
+	"license" : "LICENSE.LGPLv21",
+
+	"environment" : {
+
+		"LD_LIBRARY_PATH" : "{buildDir}/lib",
+
+	},
+
+	"variables" : {
+
+		# Make sure we pick up the python headers from {buildDir},
+		# rather than any system level headers. We refer to this
+		# variable in "commands" below.
+		"pythonIncludeDir" : "{buildDir}/include/python2.7",
+
+	},
+
+	"commands" : [
+
+		"./configure"
+			" -prefix {buildDir}"
+			" -plugindir {buildDir}/qt/plugins"
+			" -release"
+			" -opensource -confirm-license"
+			" -no-rpath"
+			" -no-dbus"
+			" -skip qtconnectivity"
+			" -skip qtwebengine"
+			" -skip qt3d"
+			" -skip qtdeclarative"
+			" -no-libudev"
+			" -no-icu"
+			" -qt-pcre"
+			" -nomake examples"
+			" -nomake tests"
+			" {extraArgs}"
+			" -I {buildDir}/include -I {buildDir}/include/freetype2"
+			" -L {buildDir}/lib",
+
+		"make -j {jobs} && make install",
+
+	],
+
+	"platform:osx" : {
+
+		"variables" : {
+
+			"extraArgs" : "-no-freetype",
+
+		},
+
+	},
+
+	"platform:linux" : {
+
+		"variables" : {
+
+			"extraArgs" : "-qt-xcb",
+
+		},
+
+	},
+
+}
