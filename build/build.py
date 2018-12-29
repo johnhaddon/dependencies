c4d50f9ed68cd4fb4f7eede7fbd0207bb8cc87bc
John Haddon <thehaddonyoof@gmail.com>
2018-12-29 19:17:35 +0000

    Build projects in order using dependency information

--- build/build.py
+++ build/build.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+import functools
 import glob
 import os
 import multiprocessing
@@ -11,18 +12,7 @@ import sys
 def __projects() :
 
 	configFiles = glob.glob( "*/config.py" )
-	return [ os.path.split( f )[0] for f in configFiles ]
-
-def __decompress( archive ) :
-
-	command = "tar -xvf {archive}".format( archive=archive )
-	sys.stderr.write( command + "\n" )
-	files = subprocess.check_output( command, stderr=subprocess.STDOUT, shell = True )
-	files = [ f for f in files.split( "\n" ) if f ]
-	files = [ f[2:] if f.startswith( "x " ) else f for f in files ]
-	dirs = { f.split( "/" )[0] for f in files }
-	assert( len( dirs ) ==  1 )
-	return next( iter( dirs ) )
+	return sorted( [ os.path.split( f )[0] for f in configFiles ] )
 
 def __loadConfig( project, buildDir ) :
 
@@ -76,9 +66,39 @@ def __loadConfig( project, buildDir ) :
 
 	return __substitute( config )
 
-def __buildProject( project, buildDir ) :
+def __loadConfigs( buildDir ) :
 
-	config = __loadConfig( project, buildDir )
+	result = {}
+	for project in __projects() :
+		result[project] = __loadConfig( project, buildDir )
+
+	return result
+
+def __decompress( archive ) :
+
+	command = "tar -xvf {archive}".format( archive=archive )
+	sys.stderr.write( command + "\n" )
+	files = subprocess.check_output( command, stderr=subprocess.STDOUT, shell = True )
+	files = [ f for f in files.split( "\n" ) if f ]
+	files = [ f[2:] if f.startswith( "x " ) else f for f in files ]
+	dirs = { f.split( "/" )[0] for f in files }
+	assert( len( dirs ) ==  1 )
+	return next( iter( dirs ) )
+
+def __preserveCurrentDirectory( f ) :
+
+	@functools.wraps( f )
+	def decorated( *args, **kw ) :
+		d = os.getcwd()
+		try :
+			return f( *args, **kw )
+		finally :
+			os.chdir( d )
+
+	return decorated
+
+@__preserveCurrentDirectory
+def __buildProject( project, config, buildDir ) :
 
 	archiveDir = project + "/archives"
 	if not os.path.exists( archiveDir ) :
@@ -131,12 +151,31 @@ def __buildProject( project, buildDir ) :
 			os.remove( link[0] )
 		os.symlink( link[1], link[0] )
 
+def __buildProjects( projects, configs, buildDir ) :
+
+	built = set()
+	def walk( project, configs, buildDir ) :
+
+		if project in built :
+			return
+
+		for dependency in configs[project].get( "dependencies", [] ) :
+			walk( dependency, configs, buildDir )
+
+		__buildProject( project, configs[project], buildDir )
+		built.add( project )
+
+	for project in projects :
+		walk( project, configs, buildDir )
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-	"--project",
+	"--projects",
 	choices = __projects(),
-	help = "The project to build."
+	nargs = "+",
+	default = __projects(),
+	help = "The projects to build."
 )
 
 parser.add_argument(
@@ -146,4 +185,6 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
-__buildProject( args.project, args.buildDir )
+
+configs = __loadConfigs( args.buildDir )
+__buildProjects( args.projects, configs, args.buildDir )
